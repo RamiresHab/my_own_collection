51383ce9692a6b9588a4037b49c45c51af2628c3
Roman Khabibullin <roman-habibullin@yandex.ru>
2023-02-24 18:12:04 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Ansible Collection - my_own_namespace.yandex_cloud_elk
+
+## Documentation for the collection.
+This is homework to netology. We have a role test_role and a module my_own_module in this collection.
+
+## Playbook
+Test playbook starts with command ```ansible-playbook site.yml```
+
+### Roles
+test_role has variables in role/test/role/vars/mail.yml
+
+| Name           | Default Value | Description                     |
+| -------------- | ------------- |---------------------------------|
+| `path_file` | "/tmp/text" | Path to file for recording text |
+| `content_file` | "Hello" | Text recording to file          |
+
+## Modules
+my_own_module is in plugins/modules/my_own_module.py
+It has description in plugins/modules/README.md
---
+++ plugins/modules/my_own_module.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python
+
+# Copyright: (c) 2018, Terry Jones <terry.jones@example.org>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = r'''
+---
+module: my_own_module
+
+short_description: This is my test module for netology homework
+
+# If this is part of a collection, you need to use semantic versioning,
+# i.e. the version is of the form "2.5.0" and not "2.4".
+version_added: "1.0.0"
+
+description: This is my longer description explaining my test module.
+
+options:
+    path:
+        description: This is file path.
+        required: true
+        type: str
+    content:
+        description: text to write into file.
+        required: true
+        type: str
+# Specify this value according to your collection
+# in format of namespace.collection.doc_fragment_name
+extends_documentation_fragment:
+    - my_namespace.my_collection.my_doc_fragment_name
+
+author:
+    - Roman Khabibullin
+'''
+
+EXAMPLES = r'''
+# Pass in a message
+- name: Test with a message
+  my_namespace.my_collection.my_test:
+    path: /path/to/file
+    context: Hello
+
+# fail the module
+- name: Test failure of the module
+  my_namespace.my_collection.my_test:
+    path: fail me
+'''
+
+RETURN = r'''
+# These are examples of possible return values, and in general should use other names for return values.
+original_message:
+    description: The original name param that was passed in.
+    type: str
+    returned: always
+    sample: 'hello world'
+message:
+    description: The output message that the test module generates.
+    type: str
+    returned: always
+    sample: 'goodbye'
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+
+
+def run_module():
+    # define available arguments/parameters a user can pass to the module
+    module_args = dict(
+        path=dict(type=str, required=True),
+        content=dict(type=str, required=True)
+    )
+
+    # seed the result dict in the object
+    # we primarily care about changed and state
+    # changed is if this module effectively modified the target
+    # state will include any data that you want your module to pass back
+    # for consumption, for example, in a subsequent task
+    result = dict(
+        changed=False,
+        original_message='',
+        message=''
+    )
+
+    # the AnsibleModule object will be our abstraction working with Ansible
+    # this includes instantiation, a couple of common attr would be the
+    # args/params passed to the execution, as well as if the module
+    # supports check mode
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    params = module.params
+    path = params['path']
+    content = params['content']
+
+    my_file = open(path, "w+")
+    my_file.write(content)
+    my_file.close()
+
+    # if the user is working with this module in only check mode we do not
+    # want to make any changes to the environment, just return the current
+    # state with no modifications
+    if module.check_mode:
+        module.exit_json(**result)
+
+    # manipulate or modify the state as needed (this is going to be the
+    # part where your module will do what it needs to do)
+    result['original_message'] = module.params['path']
+    result['message'] = module.params['content']
+
+    # during the execution of the module, if there is an exception or a
+    # conditional state that effectively causes a failure, run
+    # AnsibleModule.fail_json() to pass in the message and the result
+    if module.params['path'] == 'fail me':
+        module.fail_json(msg='You requested this to fail', **result)
+
+    # in the event of a successful module execution, you will want to
+    # simple AnsibleModule.exit_json(), passing the key/value results
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
